f86b357bf37a4e03bf06933cfc8d0f97aa734a8e
Martha Plantz <maplantz89@gmail.com>
2019-09-07 16:57:23 -0700

    adding animals

--- petful-server/src/adoptCat/adoptCatRouter.js
+++ petful-server/src/adoptCat/adoptCatRouter.js
@@ -6,6 +6,42 @@ adoptCatRouter
   .route('/')
   .get((req,res) => {
     let currentCat = catQ.dequeue();
+    catQ.enqueue({
+      imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+      imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+      name: 'Fluffy',
+      sex: 'Female',
+      age: 2,
+      breed: 'Bengal',
+      story: 'Thrown on the street'
+    });
+    catQ.enqueue({
+      imageURL:'https://www.thehappycatsite.com/wp-content/uploads/2017/09/oil.jpg', 
+      imageDescription: 'Orange bengal cat perched on the fence.',
+      name: 'Meeko',
+      sex: 'Male',
+      age: 2,
+      breed: 'Bengal',
+      story: 'Thrown on the street'
+    });
+    catQ.enqueue({
+      imageURL:'https://media.mnn.com/assets/images/2018/02/AdorableBlackCatLookingAtCameraFromSofa.jpg.653x0_q80_crop-smart.jpg', 
+      imageDescription: 'Black cat lounging on bed.',
+      name: 'Momo',
+      sex: 'Male',
+      age: 0.5,
+      breed: 'Bombay',
+      story: 'Ready to be adopted'
+    });
+    catQ.enqueue({
+      imageURL:'https://kittentoob.com/wp-content/uploads/2018/01/Mackerel-tabby-750x441.jpg', 
+      imageDescription: 'Black and grey getting ready for a cat nap.',
+      name: 'Bash',
+      sex: 'Male',
+      age: 4,
+      breed: 'Tabby',
+      story: 'Family moved'
+    });
     if(currentCat){
       adoptedAnimalsQ.push(currentCat);
     }
--- petful-server/src/adoptDog/adoptDogRouter.js
+++ petful-server/src/adoptDog/adoptDogRouter.js
@@ -6,6 +6,33 @@ adoptDogRouter
   .route('/')
   .get((req,res) => {
     let currentDog = dogQ.dequeue();
+    dogQ.enqueue({
+      imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+      imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+      name: 'Zeus',
+      sex: 'Male',
+      age: 3,
+      breed: 'Golden Retriever',
+      story: 'Owner Passed away'
+    });
+    dogQ.enqueue({
+      imageURL: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/19160330/Havanese-puppy.jpg',
+      imageDescription: 'A smiling brown puppy in a field.',
+      name: 'Havanese',
+      sex: 'Male',
+      age: 3,
+      breed: '',
+      story: 'Owner Passed away'
+    });
+    dogQ.enqueue({
+      imageURL: 'https://vetstreet-brightspot.s3.amazonaws.com/cf/d50ed0aec211e088ab0050568d634f/file/Chug-2-645km071411.jpg',
+      imageDescription: 'A smiling white coat ready for a walk.',
+      name: 'Cedrik',
+      sex: 'Male',
+      age: 3,
+      breed: 'Chihuahua Pug',
+      story: 'Owner Passed away'
+    });
     if(currentDog){
       adoptedAnimalsQ.push(currentDog);
     }
